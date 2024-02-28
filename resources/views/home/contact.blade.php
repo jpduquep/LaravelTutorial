a2859566e1e0f1b40bd21beba00d5f5ade7c530b
jpduquep <jpduquep@eafit.edu.co>
2024-02-27 20:37:25 -0500

    Commit inicial

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', 'Home Page - Online Store')
+
+@section('content')
+<div class="text-center">
+  Email: jpduquep@EAFIT
+</div>
+<div class="text-center">
+  Carrera Willy wonka
+</div>
+<div class="text-center">
+  01 800 2112
+</div>
+@endsection
